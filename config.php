5af5e201172b1a65cf4a40e473ef3a391e7ee6ba
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-07-04 09:22:51 +0200

    New folder structure

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define("BASE_DIR", str_replace(DIRECTORY_SEPARATOR, '/', realpath(dirname(__FILE
 function getDBUser(){ return "root";}
 function getPass(){ return "";}
 function getHost(){ return "localhost";}
-function getDB(){ return "labmin";}
+function getDB(){ return "labmin_tests";}
 
 include_once(BASE_DIR."Connections/conn.php");
 ?>
